0242022d0c5757187adf59b8595c715daa2c27c9
Trey927 <lulugodin12@gmail.com>
2025-05-31 13:57:09 +0200

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
